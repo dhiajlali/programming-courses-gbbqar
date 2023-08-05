34aa4096f4dcc1955efa76008f9df6616a460cda
dhiajlali <135439641+dhiajlali@users.noreply.github.com>
2023-08-05 21:38:48 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,8 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let dhia ; number;
+dhia = parselnt(prompt("enter number superieur a 100"));
+while ( dhia < 100){dhia=parselnt(prompt(enter number superieur a 100))};
+console.log(dhia);
